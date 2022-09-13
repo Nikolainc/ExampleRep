d0e3fe5516b0f7731a913bbc6b14c23c080aa8d0
nikolainc <nikolai9710@gmail.com>
2022-09-13 19:38:44 +0300

    ignore

--- Lessons/Hellouser/Program.cs
+++ Lessons/Hellouser/Program.cs
@@ -2,8 +2,8 @@
 {
     private static void Main(string[] args)
     {
-        System.Console.Write("Ваше имя: ");
+        Console.Write("Ваше имя: ");
         string name = Console.ReadLine();
-        System.Console.WriteLine($"Hello, {name}");
+        Console.WriteLine($"Hello, {name}");
     }
 }
