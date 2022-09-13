a260521acc1190441f036f71d4166003b3b2563a
nikolainc <nikolai9710@gmail.com>
2022-09-13 19:34:10 +0300

    Добавил еще один проект

---
+++ Lessons/Hellouser/Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        System.Console.Write("Ваше имя: ");
+        string name = Console.ReadLine();
+        System.Console.WriteLine($"Hello, {name}");
+    }
+}
